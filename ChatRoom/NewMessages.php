eb9c3eae6a1030196940ca8a0e5a60ee0b81936d
Genesis <genesisyau@hotmail.com>
2017-12-17 03:16:46 +0800

    changed inputs to button type
    echoed the whole html with only one echo statement, added css, changed the input reset and submit to button

--- ChatRoom/NewMessages.php
+++ ChatRoom/NewMessages.php
@@ -29,14 +29,59 @@ if(ISSET($_POST['message']))
 
 }
 
-echo '<html>';
-echo '<head></head><body>';
-echo '<form action="NewMessages.php" method="POST">';
-echo '<input type="text" name="message"/><br><br>';
-//echo '<textarea name="message" row="3" cols"50"></textarea>';
-echo '<input type="submit" value="Send"/>';
-echo '<input type="reset" value="Reset"/>';
-//echo '<input type="button" value="Delete" onclick=/>';
-echo '</form>';
-echo '</body></html>';
+echo '<html><head>
+<style type="text/css">
+  html{
+    margin:0;
+    padding:0;
+  }
+
+  button{
+    margin: 5px 10px;
+    background: #C8E77B;
+    background-image: linear-gradient(to bottom, #B6D270, #74B496);
+    border-radius: 9px;
+    font-family: Helvetica;
+    color: #ffffff;
+    font-size: 15px;
+    padding: 11px 15px;
+    border: none;
+    float: right;
+    width: 150px;
+  }
+
+  button:hover{
+    background: #74B496;
+    background-image: linear-gradient(to bottom, #74B496, #B6D270);
+  }
+
+  body{
+    margin:0;
+    width:100%;
+  }
+
+  form{
+    margin:0;
+  }
+
+  input{
+    margin: 10px 5px 10px 5px;
+    width: 98%;
+    height: 60%;
+    border-radius: 4px;
+    border: 2px solid #74B496;
+  }
+
+  input:focus{
+    outline:none;
+  }
+
+</style>
+</head><body>
+<form action="NewMessages.php" method="POST">
+<input type="text"autocomplete="off" name="message"/>
+<button type="reset" value="Reset"/>Reset</button>
+<button type="submit" value="Send"/>Send</button>
+</form>
+</body></html>';
 ?>
