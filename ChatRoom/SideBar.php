3636c9ab69bfa5b7062784f05a3c85296f05a24f
Genesis <genesisyau@hotmail.com>
2017-12-16 21:46:18 +0800

    added CSS to sidebar

--- ChatRoom/SideBar.php
+++ ChatRoom/SideBar.php
@@ -1,5 +1,12 @@
 <?php
   session_start();
-  echo "My name is:</br>";
-  echo $_SESSION['uid'];
+  echo "<p style='color: #74B496;
+  font-size: 20px;
+  font-family: Helvetica;
+  margin: 0;'>Welcome! Your username is: </p>";
+  echo "<p style='color: #B6D270;
+  font-size: 15px;
+  font-family: Helvetica;
+  font-weight: bold;
+  margin:0;'>" .$_SESSION['uid']. "</p>";
 ?>
