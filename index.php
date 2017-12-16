874884b9c404faa59ff4b014410ad67334e2bee5
Genesis <genesisyau@hotmail.com>
2017-12-16 20:27:00 +0800

    added CSS to index.php
    Added a welcome msg, added CSS to the main page

--- index.php
+++ index.php
@@ -2,18 +2,68 @@
 <html>
 
   <head>
-    <title>ChatRoom</title>
+    <title>~Chat Room~</title>
     <meta charset="UTF-16"/>
+<style type="text/css">
+        body {
+          background-color: #fff;
+          text-align: center;
+        }
+
+        html{
+          margin: auto;
+        }
+
+        button{
+          margin: 20px auto;
+          background: #C8E77B;
+          background-image: linear-gradient(to bottom, #B6D270, #74B496);
+          border-radius: 9px;
+          font-family: Helvetica;
+          color: #ffffff;
+          font-size: 15px;
+          padding: 11px 15px;
+          border: none;
+        }
+
+        button:hover{
+          background: #74B496;
+          background-image: linear-gradient(to bottom, #74B496, #B6D270);
+        }
+
+        p{
+          font-family: helvetica;
+          font-size: 13px;
+        }
+
+        .welcome-msg{
+          color: #74B496;
+          font-size: 25px;
+          font-family: Helvetica;
+          font-style: oblique;
+        }
+
+        input{
+          margin-bottom: 10px;
+          border: none;
+          border-bottom: 2px solid #E196A2;
+          width: auto;
+        }
+
+      </style>
+
   </head>
 
   <body>
     <center>
+      <p class= "welcome-msg"> Welcome to our chat room! </p>
       <form action="ChatRoom/includes/checkInfo.php" method="POST">
         <input type="text" name="uid" placeholder="Username"><br>
         <input type="password" name="pwd" placeholder="Password"><br>
         <button type="submit" name="submit">Sign in</button>
       </form>
-      <a href = "ChatRoom/signup.php"><p style="font-size:15px">signup</p></a>
+      <p>Don't have an account?
+      <a href = "ChatRoom/signup.php">Sign Up</a></p>
     </center>
   </body>
 
