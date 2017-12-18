bdb6cb58c707351fd49dfe79b7ddb0514f10e12b
drosaslin <33602982+drosaslin@users.noreply.github.com>
2017-12-18 13:50:07 +0800

    sign up and sign in information check
    Now checks if the user has provided all the information in sign up and sign in

--- ChatRoom/includes/insertInfo.php
+++ ChatRoom/includes/insertInfo.php
@@ -1,16 +1,22 @@
 <?php
   include_once ("dbms.php");
 
-  $first = mysqli_real_escape_string($conn, $_POST['first']);
-  $last = mysqli_real_escape_string($conn, $_POST['last']);
-  $email = mysqli_real_escape_string($conn, $_POST['email']);
-  $uid = mysqli_real_escape_string($conn, $_POST['uid']);
-  $password = mysqli_real_escape_string($conn, md5($_POST['pwd']));
+  if(isset($_POST['submit']) && !empty($_POST['first']) && !empty($_POST['last']) && !empty($_POST['email']) && !empty($_POST['uid']) && !empty($_POST['pwd']))
+  {
+    $first = mysqli_real_escape_string($conn, $_POST['first']);
+    $last = mysqli_real_escape_string($conn, $_POST['last']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+    $password = mysqli_real_escape_string($conn, md5($_POST['pwd']));
 
-  $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
-          VALUES ('$first', '$last', '$email', '$uid', '$password');";
+    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+            VALUES ('$first', '$last', '$email', '$uid', '$password');";
 
-  mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql);
+    header("location: ../../index.php?signup=success");
+  }
+  else
+    header("location: ../signup.php?signup=unsuccessful");
 
-  header("location: ../../index.php?signup=success");
+  mysqli_close($conn);
  ?>
